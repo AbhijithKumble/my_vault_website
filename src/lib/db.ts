09922fc425bfd1343caac8c81141ac64dccc2de1
AbhijithKumble <abhijithkumble87564@gmail.com>
2026-09-23 00:17:38 +0530

    fixed something

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { createClient, type Client } from "@libsql/client";
+import { createClient, type Client } from "@libsql/client/web";
 
 let cachedClient: Client | null = null;
 let cachedKey: string | null = null;
